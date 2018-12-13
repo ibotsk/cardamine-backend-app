086663b0207b45f144302684c47a0456afe3a629
Matus Kempa <matus.kempa@gmail.com>
2018-12-13 19:16:25 +0100

    Discover models script with json and js models

--- utils/discover-models.js
+++ utils/discover-models.js
@@ -8,7 +8,7 @@ const readFile = promisify(fs.readFile);
 const mkdirp = promisify(require('mkdirp'));
 
 const DATASOURCE_NAME = 'cardamine';
-const dataSourceConfig = require('../datasources.json');
+const dataSourceConfig = require('../server/datasources.json');
 const db = new loopback.DataSource(dataSourceConfig[DATASOURCE_NAME]);
 
 const models = [
@@ -37,6 +37,14 @@ discover(models).then(
     error => { console.error('UNHANDLED ERROR:\n', error); process.exit(1); },
 );
 
+function makeJsModel(modelName) {
+    const modelNameNoSpaces = modelName.replace(/\_/g, '');
+    const modelNameFirstCapital = modelNameNoSpaces.charAt(0).toUpperCase() + modelNameNoSpaces.slice(1);
+    const contents = `'use strict'\n\nmodule.exports = function(${modelNameFirstCapital}) {\n\n};\n`;
+
+    return contents;
+}
+
 async function discover(models) {
     // It's important to pass the same "options" object to all calls
     // of dataSource.discoverSchemas(), it allows the method to cache
@@ -45,29 +53,43 @@ async function discover(models) {
 
     // Discover models and relations
 
-    await mkdirp('../../common/models');
+    await mkdirp('../common/models');
 
     const schemas_config = {};
     for (const model of models) {
         const schema = await db.discoverSchemas(model, options);
 
-        const modelDashed = model.replace('_', '-');
+        const modelDashed = model.replace(/\_/g, '-');
+        const schemaModel = schema[`public.${model}`];
+
+        schemaModel.base = "PersistedModel";
+        schemaModel.name = model;
+
+        const filePath = `../common/models/${modelDashed}`;
+        // write json file
         await writeFile(
-            `../../common/models/${modelDashed}.json`,
-            JSON.stringify(schema[`public.${model}`], null, 2)
+            `${filePath}.json`,
+            JSON.stringify(schemaModel, null, 2)
         );
+        // write js file
+        await writeFile(
+            `${filePath}.js`,
+            makeJsModel(model)
+        );
+
         schemas_config[model] = { dataSource: DATASOURCE_NAME, public: true };
 
     }
 
     // Expose models via REST API
-    const configJson = await readFile('/home/matus/Documents/Projects/VSCode/cardamine-backend-app/server/model-config.json', 'utf-8');
+    const modelConfigFile = '../server/model-config.json';
+    const configJson = await readFile(modelConfigFile, 'utf-8');
     console.log('MODEL CONFIG', configJson);
     const config = JSON.parse(configJson);
     const mergedConfig = {...config, ...schemas_config};
 
     await writeFile(
-      '/home/matus/Documents/Projects/VSCode/cardamine-backend-app/server/model-config.json',
+        modelConfigFile,
       JSON.stringify(mergedConfig, null, 2)
     );
 }
