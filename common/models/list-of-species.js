74a132bf3f03f08b70e40b7d2ac3bf417d2c9b9c
Matus Kempa <matus.kempa@gmail.com>
2019-01-06 14:55:39 +0100

    Before save on list of species sets order

--- common/models/list-of-species.js
+++ common/models/list-of-species.js
@@ -1,5 +1,33 @@
 'use strict'
 
-module.exports = function(Listofspecies) {
+const ntypeOrderMap = (ntype) => {
+    switch (ntype) {
+        case "A":
+        case "PA":
+            return 1;
+        case "S":
+        case "DS":
+            return 3;
+        case "U":
+            return 5;
+        default:
+            return 8;
+    }
+}
+
+module.exports = function (Listofspecies) {
+
+    Listofspecies.beforeSave = function (next, modelInstance) {
+
+        if (modelInstance.hybrid) {
+            modelInstance.ntype = 'H';
+        }
+        if (modelInstance.synType && parseInt(modelInstance.synType) === 1) {
+            modelInstance.ntypeOrder = 4;
+        } else {
+            modelInstance.ntypeOrder = ntypeOrderMap(modelInstance.ntype);
+        }
+        next();
+    };
 
 };
