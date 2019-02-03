58b9a56748048b0fe9784dfcc1e9be433edf769b
Matus Kempa <matus.kempa@gmail.com>
2019-02-03 12:00:07 +0100

    Before save does not set H for hybrid

--- common/models/list-of-species.js
+++ common/models/list-of-species.js
@@ -19,9 +19,6 @@ module.exports = function (Listofspecies) {
 
     Listofspecies.beforeSave = function (next, modelInstance) {
 
-        if (modelInstance.hybrid) {
-            modelInstance.ntype = 'H';
-        }
         if (modelInstance.synType && parseInt(modelInstance.synType) === 1) {
             modelInstance.ntypeOrder = 4;
         } else {
