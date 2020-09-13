0e079b4bad79c9cce1f2a454f295b9fbd3717815
Matus Kempa <matus.kempa@gmail.com>
2020-09-13 19:54:58 +0200

    Parsing of geography coordinates with wkx library

--- common/models/material.js
+++ common/models/material.js
@@ -1,5 +1,26 @@
-'use strict'
+'use strict';
+
+const wkx = require('wkx');
+
+const geogHexToJSON = (hexCoord) => {
+  const wkbBuffer = Buffer.from(hexCoord, 'hex');
+  const geom = wkx.Geometry.parse(wkbBuffer);
+
+  return geom.toGeoJSON();
+};
 
 module.exports = function(Material) {
+  Material.observe('loaded', (ctx, next) => {
+    const data = ctx.data;
+    if (data) {
+      if (data.coordinatesGeoref) {
+        data.coordinatesGeoref = geogHexToJSON(data.coordinatesGeoref);
+      }
+      if (data.coordinatesForMap) {
+        data.coordinatesForMap = geogHexToJSON(data.coordinatesForMap);
+      }
+    }
 
+    return next();
+  });
 };
