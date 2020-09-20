7e0f88182ead1ab7157a09379b3fed38ca3fee49
Matus Kempa <matus.kempa@gmail.com>
2020-09-20 16:26:52 +0200

    Saving georef and map coordinates as geography

--- common/models/material.js
+++ common/models/material.js
@@ -20,6 +20,25 @@ const geogHexToJSON = (hexCoord) => {
   };
 };
 
+/**
+ *
+ * @param {string} coordinatesString stringified JSON that must contain lat and lon
+ */
+const geogJSONToHex = (coordinatesString) => {
+  const {coordinates} = JSON.parse(coordinatesString);
+  const {lat, lon} = coordinates;
+
+  if (!lat || !lon) {
+    throw new Error(
+      `Both latitude and longitude must be provided. Was lat=${lat} lon=${lon}`
+    );
+  }
+
+  // TODO: default SRID is 4326 but the lib doesn't take any other SRID into account
+  const wkb = new wkx.Point(lon, lat).toWkb();
+  return wkb.toString('hex');
+};
+
 module.exports = function(Material) {
   Material.observe('loaded', (ctx, next) => {
     const data = ctx.data;
@@ -30,6 +49,27 @@ module.exports = function(Material) {
       if (data.coordinatesForMap) {
         data.coordinatesForMap = geogHexToJSON(data.coordinatesForMap);
       }
+    }
+
+    return next();
+  });
+
+  // coordinatesGeoref and coordinatesForMap of input object must be a stringified JSON with coordinates
+  Material.observe('before save', (ctx, next) => {
+    const {instance} = ctx;
+    if (!instance) {
+      throw new Error('No instance to save');
+    }
+
+    if (instance.coordinatesGeoref) {
+      instance.coordinatesGeoref = geogJSONToHex(
+        instance.coordinatesGeoref
+      );
+    }
+    if (instance.coordinatesForMap) {
+      instance.coordinatesForMap = geogJSONToHex(
+        instance.coordinatesForMap
+      );
     }
 
     return next();
